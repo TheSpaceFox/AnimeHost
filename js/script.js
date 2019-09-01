ea35b23421b0a3707c94aee6efdd19ddded06ad6
TheSpaceFox <54290713+TheSpaceFox@users.noreply.github.com>
2019-09-01 15:33:25 -0300

    Update 1.2
    New page avaliable

---
+++ js/script.js
@@ -0,0 +1,17 @@
+    function myFunction() {
+  	var x = document.getElementById("menu");
+  	if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+	function myFunction2() {
+  	var y = document.getElementById("form");
+  	if (y.style.display === "block") {
+        y.style.display = "none";
+    } else {
+        y.style.display = "block";
+    }
+}
