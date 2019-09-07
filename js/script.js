e711c1cdfaf1d6f099f0882d05e9d7a18838fbf1
TheSpaceFox <54290713+TheSpaceFox@users.noreply.github.com>
2019-09-06 23:26:26 -0300

    Beta with firsts bugs :)
    Bugs wild appeared

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
-    function myFunction() {
+function myFunction() {
   	var x = document.getElementById("menu");
+    var y = document.getElementById("form");
   	if (x.style.display === "block") {
         x.style.display = "none";
     } else {
         x.style.display = "block";
+    y.style.display = "none";
     }
 }
 
-	function myFunction2() {
+function myFunction2() {
   	var y = document.getElementById("form");
+    var x = document.getElementById("menu");
   	if (y.style.display === "block") {
         y.style.display = "none";
     } else {
         y.style.display = "block";
+    x.style.display = "none";
     }
 }
